8ba64c8737871ee322caaf03e0a02970e47ba249
PIPAPUTZEMANN <sebi-brenner@web.de>
2017-10-29 19:04:14 +0100

    test

---
+++ Labor-2-/src/Lotto.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+
+public class Lotto {
+
+	public static void main(String[] args) {
+
+		System.out.println(drawLotteryNumber());
+		System.out.println(Arrays.toString(drawLottery()));
+		readPlayerTip();
+	}
+
+	public static int drawLotteryNumber() {
+
+		int zufallLottoZahl;
+		zufallLottoZahl = (int) (Math.random() * 49) + 1;
+
+		return zufallLottoZahl;
+
+	}
+
+	public static int[] drawLottery() {
+
+		int[] ZiehungZahlen;
+		ZiehungZahlen = new int[6];
+
+		for (int b = 0; b < ZiehungZahlen.length; b++) {
+
+			ZiehungZahlen[b] = drawLotteryNumber();
+
+			for (int i = 0; i < b; i++) {
+				while (ZiehungZahlen[b] == ZiehungZahlen[i]) {
+
+					ZiehungZahlen[b] = drawLotteryNumber();
+					i = 0;
+
+				}
+
+			}
+		}
+		return ZiehungZahlen;//
+	}
+
+	public static int[] readPlayerTip() {
+
+		int[] SpielerZahlen;
+		SpielerZahlen = new int[6];
+		int ZwischenSpeicher;
+		boolean bereich = true;
+
+		for (int s = 0; s < SpielerZahlen.length; s++) {
+			System.out.println("Bitte die " + (s + 1) + ". Zahl eingeben!");
+			ZwischenSpeicher = In.readInt();
+			while(bereich==true){
+			if(ZwischenSpeicher <= 49 && ZwischenSpeicher >= 1) {
+
+				SpielerZahlen[s] = ZwischenSpeicher;
+				bereich=false;
+			
+			}
+			else{
+				System.out.println("Die eingegebene Zahl ist nicht gültig! Bitte die " +(s+1)+ ". nochmal eingeben!");
+				ZwischenSpeicher = In.readInt();
+				bereich=true;
+			
+			}
+			
+		}}
+		return SpielerZahlen;
+
+	}
+}
