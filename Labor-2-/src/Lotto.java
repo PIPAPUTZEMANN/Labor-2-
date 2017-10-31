3df3515dea201741d0aba81d05630e7ea2aee2e5
PIPAPUTZEMANN <sebi-brenner@web.de>
2017-10-31 21:44:57 +0100

    läuft 2 dimensionales array

--- Labor-2-/src/Lotto.java
+++ Labor-2-/src/Lotto.java
@@ -3,38 +3,23 @@ import java.util.Arrays;
 public class Lotto {
 
 	public static void main(String[] args) {
-		int k=0;
-		int [] array = null;
+		int anzahlSpieler = 0;
+
 		System.out.println("Anzahl der gewünschten Spieler eingeben!");
-		int AnzahlSpieler=In.readInt();
-		for(int as=0;as<AnzahlSpieler;as++){
-			
-			System.out.println("Spieler "+(as+1)+" bitte seinen Lottoschein ausfüllen!");
-			array=readPlayerTip();}
-			
-		while(checkPlayerTip(array,drawLottery() )!=6){
-			
-			
-			drawLottery();
-			k++;
-		}
-		System.out.println(k);
-		}
-		
+		anzahlSpieler = In.readInt();
 
-		
-		
-		
-		
-		
-		
-		//int[] test = { 1, 2, 3, 4, 5, 6 };
-		// System.out.println(drawLotteryNumber());
-		//System.out.println(Arrays.toString(drawLottery()));
-		// System.out.println(Arrays.toString(readPlayerTip()));
-		//System.out.println(checkPlayerTip(readPlayerTip(), test));
+		for (int as = 0; as < anzahlSpieler; as++) {
+			System.out.println("Spieler " + (as + 1) + " bitte seinen Lottoschein ausfüllen!");
+			int[][] aa = readPlayerTip(anzahlSpieler);
 
-	
+		}
+	}
+
+	// int[] test = { 1, 2, 3, 4, 5, 6 };
+	// System.out.println(drawLotteryNumber());
+	// System.out.println(Arrays.toString(drawLottery()));
+	// System.out.println(Arrays.toString(readPlayerTip()));
+	// System.out.println(checkPlayerTip(readPlayerTip(), test));
 
 	public static int drawLotteryNumber() {
 
@@ -67,51 +52,53 @@ public class Lotto {
 		return ZiehungZahlen;//
 	}
 
-	public static int[] readPlayerTip() {
+	public static int[][] readPlayerTip(int anzahlSpieler) {
 
-		int[] SpielerZahlen;
-		SpielerZahlen = new int[6];
+		int[][] SpielerZahlen;
+		SpielerZahlen = new int[anzahlSpieler][6];
 		int ZwischenSpeicher;
+
 		boolean bereich = true;
 
-		for (int s = 0; s < SpielerZahlen.length; s++) {
+		for (int s = 0; s < 6; s++) {
 			System.out.println("Bitte die " + (s + 1) + ". Zahl eingeben!");
 			ZwischenSpeicher = In.readInt();
+			for (int spielerNummer = 0; spielerNummer < anzahlSpieler; spielerNummer++) {
+				for (int i = 0; i < s; i++) {
+					while (ZwischenSpeicher == SpielerZahlen[spielerNummer][i]) {
 
-			for (int i = 0; i < s; i++) {
-				while (ZwischenSpeicher == SpielerZahlen[i]) {
+						System.out.println("Zahl Doppelt! Bitte erneut die " + (i + 2) + ".eingeben!");
+						ZwischenSpeicher = In.readInt();
 
-					System.out.println("Zahl Doppelt! Bitte erneut die " + (i + 2) + ".eingeben!");
-					ZwischenSpeicher = In.readInt();
-
-					i = 0;
+						i = 0;
+					}
 				}
-			}
-
-			if (ZwischenSpeicher <= 49 && ZwischenSpeicher >= 1) {
-
-				bereich = false;
-
-			} else {
-
-				bereich = true;
-
-			}
-
-			while (bereich == true) {
-
-				System.out
-						.println("Die eingegebene Zahl ist nicht gültig! Bitte die " + (s + 1) + ". nochmal eingeben!");
-				ZwischenSpeicher = In.readInt();
 
 				if (ZwischenSpeicher <= 49 && ZwischenSpeicher >= 1) {
+
 					bereich = false;
+
+				} else {
+
+					bereich = true;
+
 				}
 
+				while (bereich == true) {
+
+					System.out.println(
+							"Die eingegebene Zahl ist nicht gültig! Bitte die " + (s + 1) + ". nochmal eingeben!");
+					ZwischenSpeicher = In.readInt();
+
+					if (ZwischenSpeicher <= 49 && ZwischenSpeicher >= 1) {
+						bereich = false;
+					}
+
+				}
+
+				SpielerZahlen[spielerNummer][s] = ZwischenSpeicher;
+
 			}
-
-			SpielerZahlen[s] = ZwischenSpeicher;
-
 		}
 		return SpielerZahlen;
 
@@ -120,8 +107,8 @@ public class Lotto {
 	public static int checkPlayerTip(int[] tip, int[] lotteryNumbers) {
 		int richtigeZahlen = 0;
 
-		//System.out.println(Arrays.toString(tip));
-		//System.out.println(Arrays.toString(lotteryNumbers));
+		// System.out.println(Arrays.toString(tip));
+		// System.out.println(Arrays.toString(lotteryNumbers));
 		for (int j = 0; j < tip.length; j++) {
 			for (int h = 0; h < lotteryNumbers.length; h++) {
 				if (tip[j] == lotteryNumbers[h]) {
